3a171ef0471f5ef70512abdfeaafc0cb4a6a879e
leonid22231 <lsoldatov.lenya@gmail.com>
2022-07-22 17:35:13 +0700

    Create project

---
+++ src/main/java/com/lyadev/WoodSite/WoodSiteApplication.java
@@ -0,0 +1,13 @@
+package com.lyadev.WoodSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoodSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lyadev/WoodSite/WoodSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyadev.WoodSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoodSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
