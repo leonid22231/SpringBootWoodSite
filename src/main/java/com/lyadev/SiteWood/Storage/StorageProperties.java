51bf9dc835b739c49365a3e456431875637f46fc
leonid22231 <lsoldatov.lenya@gmail.com>
2022-07-26 15:45:16 +0700

    Pred Realise

--- src/main/java/com/lyadev/SiteWood/Storage/StorageProperties.java
+++ src/main/java/com/lyadev/SiteWood/Storage/StorageProperties.java
@@ -8,7 +8,7 @@ public class StorageProperties {
     /**
      * Folder location for storing files
      */
-    private String location = "/upload-dir/app";
+    private String location = "image_dir";
 
     public String getLocation() {
         return location;
